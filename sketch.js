012b29d233482a596e8b5e3f7f1d19ff4c4ab5d0
vihaan16 <67194841+vihaan16@users.noreply.github.com>
2021-03-01 11:30:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,96 @@
+var dog, happyDog, database, foodS, foodStock
+var dogImg, dogHappyImg;
+var milk, milkImg;
+
+
+function preload()
+{
+  dogImg = loadImage("Dog.png");
+  dogHappyImg = loadImage("happydog.png");
+  milkImg = loadImage("milk.png");
+  
+
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+
+  emo = createSprite(200,200,1,1);
+  
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+  foodStock.set(50);
+  
+  milk = createSprite(140,435,10,10);
+  milk.addImage(milkImg);
+  milk.scale = 0.025;
+
+  milk1 = createSprite(210,280,10,10);
+  milk1.addImage(milkImg);
+  milk1.scale = 0.025;
+  milk1.visible = false;
+
+
+  
+}
+
+
+function draw() {  
+  background("green")
+
+  if(foodS !== 0){
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappyImg);
+    milk1.visible = true;
+
+   
+  }
+
+  if(keyWentUp(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg);
+    milk1.visible = false;
+  }
+}
+
+if(foodS == 0){
+  
+  dog.addImage(dogImg);
+  foodS = 50;
+
+}
+
+
+
+  drawSprites();
+  textSize(17);
+  fill("black");
+  text("Press up arrow key to feed milk to the Dog ",50,50);
+  fill("black");
+  text("Milk Bottles Remaining "+foodS,170,440);
+}
+
+function readStock(data)
+{
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x=x-1
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
+
